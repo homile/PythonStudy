4a1d547a895b248d7b6c2d6f04c576a26fedfd2c
Homile <ssanho2@naver.com>
2021-12-12 14:30:24 +0900

    python study 8주차 1번문제 k번째 수
    2021. 08. 13. 8주차 1번문제 k번째 수

---
+++ 2021/08.13/20210813(1).py
@@ -0,0 +1,26 @@
+# k번째수
+
+# 문제설명: 
+
+# 문제접근
+# 슬라이싱 기법을 사용한 풀이
+
+# 문제풀이(1)
+def solution(array, commands):
+    answer = [] 
+    for i in range(len(commands)):
+        answer_list=array[commands[i][0]-1:commands[i][1]]
+        answer_list.sort()
+        
+        answer.append(answer_list[commands[i][2]-1])
+        print(answer_list)
+        
+    print(array[1-1], 'c', commands[1-1][1-1])
+    return answer
+
+
+# 다른 사람의 풀이(1)
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+
+print(solution([1,5,2,6,3,7,4], [[2,5,3],[4,4,1],[1,7,3]]))     # [5,6,3]
