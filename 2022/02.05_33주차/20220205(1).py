5ed2209ace5c6a9f5e9361eef656b49d26c6c2cb
homile <ssanho2@naver.com>
2022-02-25 16:39:37 +0900

    python study 33주차 1번문제 [1차]캐시
    2022. 02. 05. 33주차 1번문제 [1차]캐시

---
+++ 2022/02.05_33주차/20220205(1).py
@@ -0,0 +1,168 @@
+# [1차] 캐시
+
+# 문제설명: https://programmers.co.kr/learn/courses/30/lessons/17680
+# 지도개발팀에서 근무하는 제이지는 지도에서 도시 이름을 검색하면 해당 도시와 
+# 관련된 맛집 게시물들을 데이터베이스에서 읽어 보여주는 서비스를 개발하고 있다.
+# 이 프로그램의 테스팅 업무를 담당하고 있는 어피치는 서비스를 오픈하기 전 
+# 각 로직에 대한 성능 측정을 수행하였는데, 제이지가 작성한 부분 중 데이터베이스에서 
+# 게시물을 가져오는 부분의 실행시간이 너무 오래 걸린다는 것을 알게 되었다.
+# 어피치는 제이지에게 해당 로직을 개선하라고 닦달하기 시작하였고, 
+# 제이지는 DB 캐시를 적용하여 성능 개선을 시도하고 있지만 캐시 크기를 얼마로 해야 
+# 효율적인지 몰라 난감한 상황이다.
+
+# 어피치에게 시달리는 제이지를 도와, DB 캐시를 적용할 때 캐시 크기에 따른 
+# 실행시간 측정 프로그램을 작성하시오.
+
+# 입력 형식
+# 캐시 크기(cacheSize)와 도시이름 배열(cities)을 입력받는다.
+# cacheSize는 정수이며, 범위는 0 ≦ cacheSize ≦ 30 이다.
+# cities는 도시 이름으로 이뤄진 문자열 배열로, 최대 도시 수는 100,000개이다.
+# 각 도시 이름은 공백, 숫자, 특수문자 등이 없는 영문자로 구성되며, 
+# 대소문자 구분을 하지 않는다. 도시 이름은 최대 20자로 이루어져 있다.
+
+# 출력 형식
+# 입력된 도시이름 배열을 순서대로 처리할 때, "총 실행시간"을 출력한다.
+
+# 조건
+# 캐시 교체 알고리즘은 LRU(Least Recently Used)를 사용한다.
+# cache hit일 경우 실행시간은 1이다.
+# cache miss일 경우 실행시간은 5이다.
+
+# 문제접근
+# LRU의 알고리즘에 대해 공부한다.
+# LRU: 가장 오래된 내역을 삭제하고 최신 내역을 삽입하는 알고리즘이다.
+# cacheSize의 값이 0일 경우 len(cacheSize) * 5를 반환한다.
+
+# 문제풀이(1)
+def solution(cacheSize, cities):
+    answer = 0
+    cache = []
+
+    # cacheSize의 값이 0 일 경우 
+    if cacheSize == 0 : return len(cities) * 5
+
+    # LRU 알고리즘 적용
+    for i in cities:
+        if not i in cache:
+            if len(cache) < cacheSize:
+                cache.append(i)
+                answer += 1
+            else:
+                cache.pop(0)
+                cache.append(i)
+                answer += 1
+        else:
+            cache.pop(0)
+            cache.append(i)
+            answer += 1
+
+    return answer
+
+# LRU 알고리즘을 적용하였으나 문제는 cities 배열을 순서대로 처리해야한다.
+# 대소문자를 구분하지 않기 때문에 소문자로 치환하여 cache에 쌓아야 한다.
+
+
+# 문제풀이(2)
+def solution(cacheSize, cities):
+    answer = 0
+    cache = []
+
+    # cacheSize의 값이 0 일 경우 
+    if cacheSize == 0 : return len(cities) * 5
+
+    # LRU 알고리즘 적용
+    for i in cities:
+        citi_low = i.lower()
+        if citi_low not in cache:
+            # cache miss
+            answer += 5
+            if len(cache) < cacheSize:
+                cache.append(citi_low)
+            else:
+                cache.pop(0)
+                cache.append(citi_low)
+        # cache hit
+        else:
+            cache.remove(citi_low)
+            cache.append(citi_low)
+            answer += 1
+
+    return answer
+
+
+# 문제풀이(3) 2, 5, 6 정답
+def solution(cacheSize, cities):
+    answer = 0
+    cache = []
+
+    # cacheSize의 값이 0 일 경우 
+    if cacheSize == 0 : 
+        return len(cities) * 5
+
+    # LRU 알고리즘 적용
+    for i in cities:
+        citi_low = i.lower()
+        # cache hit
+        if citi_low in cache:
+            cache.remove(citi_low)
+            cache.append(citi_low)
+            answer += 1
+        # cache miss
+        else:
+            cache.append(citi_low)
+            answer += 5
+
+    return answer
+
+# TC 1 = 30 / TC 3 = 44 / TC 4 = 44
+# 중복되는 코드를 제거하기 위해 if...not...in 에서 if...in으로 변경
+# cache hit과 miss 부분을 추가
+
+
+# 문제풀이(4)
+def solution(cacheSize, cities):
+    answer = 0
+    cache = []
+
+    # cacheSize의 값이 0 일 경우 
+    if cacheSize == 0 : 
+        return len(cities) * 5
+
+    # LRU 알고리즘 적용
+    for i in cities:
+        citi_low = i.lower()
+        # cache hit
+        if citi_low in cache:
+            # print('citi: ', citi_low, cache)
+            cache.remove(citi_low)
+            cache.append(citi_low)
+            # print('citi_remove: ', citi_low, cache)
+            answer += 1
+        # cache miss
+        else:
+            answer += 5
+            if len(cache) < cacheSize:
+                cache.append(citi_low)
+            else:
+                cache.pop(0)
+                cache.append(citi_low)
+
+    return answer
+
+# cache hit과 miss를 조금 더 명확하게 분리
+
+
+print(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", 
+"Seoul", "NewYork", "LA"]))    # 50
+
+print(solution(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", 
+"Pangyo", "Seoul"]))           # 21
+
+print(solution(2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", 
+"Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]))   # 60
+
+print(solution(5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", 
+"Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]))   # 52
+
+print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))        # 16
+print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))    # 25
