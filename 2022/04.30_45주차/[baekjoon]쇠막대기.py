d01d4f189c04cc139c7068a2719a3c632c9c55b7
homile <ssanho2@naver.com>
2022-05-01 21:44:21 +0900

    [baekjoon]쇠막대기 문제 등록

---
+++ 2022/04.30_45주차/[baekjoon]쇠막대기.py
@@ -0,0 +1,14 @@
+# 쇠막대기
+# 링크: https://www.acmicpc.net/problem/10799
+
+# 문제접근
+# ()는 레이저 (막대기(레이저)막대기)
+
+# 문제풀이(1)
+def solution():
+   answer = 0
+
+   return answer
+
+print(solution('()(((()())(())()))(())'))    # 17
+print(solution('(((()(()()))(())()))(()())'))    # 24
