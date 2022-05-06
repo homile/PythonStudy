02a8f3c587e6822749ae74a89426f82cf7ec832c
homile <ssanho2@naver.com>
2022-05-07 01:03:23 +0900

    [baekjoon]쇠막대기 풀이

--- 2022/04.30_45주차/[baekjoon]쇠막대기.py
+++ 2022/04.30_45주차/[baekjoon]쇠막대기.py
@@ -1,14 +1,29 @@
 # 쇠막대기
 # 링크: https://www.acmicpc.net/problem/10799
 
-# 문제접근
+# [문제접근]
 # ()는 레이저 (막대기(레이저)막대기)
 
 # 문제풀이(1)
-def solution():
-   answer = 0
+stick = input()
+answer = 0
+stack = []
 
-   return answer
+for i in range(len(stick)):
+   if stick[i] == '(':
+      stack.append(stick[i])
+   else:
+      if stick[i-1] == '(':
+         stack.pop()
+         answer += len(stack)
+      else:
+         stack.pop()
+         answer += 1
+         
+   # print(stack, answer)
 
-print(solution('()(((()())(())()))(())'))    # 17
-print(solution('(((()(()()))(())()))(()())'))    # 24
+print(answer)
+
+
+# '()(((()())(())()))(())'        # 17
+# '(((()(()()))(())()))(()())'    # 24
