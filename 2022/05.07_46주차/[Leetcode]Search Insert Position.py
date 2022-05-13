328a514b750d0eb1f4a76c19d9535d726c197a8a
homile <ssanho2@naver.com>
2022-05-14 01:29:07 +0900

    [Leetcode]Search Insert Position 풀이

---
+++ 2022/05.07_46주차/[Leetcode]Search Insert Position.py
@@ -0,0 +1,34 @@
+# Search Insert Position
+# 링크: https://leetcode.com/problems/search-insert-position/
+
+# 문제풀이(1)
+def solution(nums, target):
+   answer = 0
+   
+   for i in range(len(nums)):
+      if nums[i] == target:
+         return i
+
+   return answer
+
+# 들어오지 않는 경우 근접한 값의 인덱스번호를 +한다?
+
+
+# 문제풀이(2)
+def solution(nums, target):
+   
+   if target in nums:
+      return nums.index(target)
+
+   count = 0
+   for i in nums:
+      print(i, count)
+      if i > target:
+         return count
+      count += 1
+
+   return len(nums)
+
+print(solution([1,3,5,6], 5)) # 2
+print(solution([1,3,5,6], 7)) # 4
+print(solution([1,3,5,6], 2)) # 1
