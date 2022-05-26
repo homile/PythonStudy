ff54845f09b2f8489b71d5372c2e8f20ecd188dc
homile <ssanho2@naver.com>
2022-05-26 23:45:32 +0900

    [level3]입국심사 다른사람  풀이

--- 2022/05.14_47주차/[level3]입국심사.py
+++ 2022/05.14_47주차/[level3]입국심사.py
@@ -7,6 +7,8 @@
 # 1번(7분) -> 3번(7분) -> 5번(7분) -> 6번(7분)
 # 2번(10분) -> 4번(20분)
 
+# 이분탐색: 중간에서 범위 탐색
+
 # 문제풀이(1)
 def solution(n, times):
   answer = 0
@@ -17,8 +19,25 @@ def solution(n, times):
   right = max(times) * n
   print(left, right)
 
+  while left <= right:
+    mid = (left + right) // 2
+    check = 0
+    for time in times:
+      check += mid // time
+
+      if check >= n:
+        break
+    
+    if check >= n:
+      answer = mid
+      right = mid - 1
+    elif check < n:
+      left = mid + 1
   
   return answer
 
 
 print(solution(6, [7, 10]))   # 28
+
+
+# 참고자료: # https://happy-obok.tistory.com/10
