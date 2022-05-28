5b27b3e1f9eab7db9deb22a2fa46a49f82274b72
homile <ssanho2@naver.com>
2022-05-28 21:31:00 +0900

    [baekjoon]알파벳 개수 문제 업로드

---
+++ 2022/05.28_48주차/[baekjoon]알파벳 개수.py
@@ -0,0 +1,10 @@
+# 알파벳 개수
+# 링크: https://www.acmicpc.net/problem/10808
+
+# 문제풀이(1)
+def solution(input):
+  answer = 0
+  return answer
+
+# 1 1 0 0 1 0 0 0 0 1 1 0 0 1 2 0 0 0 0 0 0 0 0 0 0 0
+print(solution('baekjoon')) 
