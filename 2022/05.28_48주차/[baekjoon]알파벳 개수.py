fd82b5dd5011545cf4a82565db061d464532eacd
homile <ssanho2@naver.com>
2022-05-31 23:44:42 +0900

    [baekjoon]알파벳 개수 문제 풀이

--- 2022/05.28_48주차/[baekjoon]알파벳 개수.py
+++ 2022/05.28_48주차/[baekjoon]알파벳 개수.py
@@ -1,10 +1,21 @@
 # 알파벳 개수
 # 링크: https://www.acmicpc.net/problem/10808
 
+# 알파벳 개수 26개
+# 소문자로만 이루어짐
+# ord 쓰면될듯 ascii code = 97부터
+
 # 문제풀이(1)
-def solution(input):
-  answer = 0
-  return answer
+arr = input()
+
+alphabet = [0] * 26
+# print(alphabet)
+
+for i in arr:
+  alphabet[ord(i)-97] += 1
+
+print(alphabet)
+
 
 # 1 1 0 0 1 0 0 0 0 1 1 0 0 1 2 0 0 0 0 0 0 0 0 0 0 0
-print(solution('baekjoon')) 
+# input: 'baekjoon'
