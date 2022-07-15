931e390cf000565ad8dc021581f809fd9d06ac52
homile <ssanho2@naver.com>
2022-07-15 09:56:58 +0900

    [beakjoon]네 수 문제풀이 완료

---
+++ 2022/07.09_53주차/[beakjoon]네 수.py
@@ -0,0 +1,16 @@
+# 네 수
+# 링크: https://www.acmicpc.net/problem/10824
+
+# 문제풀이(1)
+num = input().split()
+
+left = int(num[0] + num[1])
+right = int(num[2] + num[3])
+
+print(left+right)
+
+# input
+# 10 20 30 40
+
+# output
+# 4060
