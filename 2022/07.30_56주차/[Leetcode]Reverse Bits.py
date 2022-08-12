92abf6716bfb2a386d51732967b7ae9b16406b5b
homile <ssanho2@naver.com>
2022-08-13 00:19:21 +0900

    수정

--- 2022/07.30_56주차/[Leetcode]Reverse Bits.py
+++ 2022/07.30_56주차/[Leetcode]Reverse Bits.py
@@ -2,14 +2,3 @@
 # 링크: https://leetcode.com/problems/reverse-bits/
 
 # 문제풀이(1)
-def solution(columnNumber):
-    # A ~ ZZ까지
-  answer = ''
-
-  while columnNumber != 0:
-    temp = chr(ord("A") + (columnNumber - 1) % 26)
-    columnNumber = (columnNumber - 1) // 26
-    answer = temp + answer
-  return answer
-
-print(solution(701))    # "ZY"
